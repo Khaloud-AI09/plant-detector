ba4a9d7c1944511d634ba81311589e8dc4698b7e
Khaloud_ <Khaloudkhalidmir@gmail.com>
2026-01-04 19:21:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ from PIL import Image, ImageOps
 import numpy as np
 
 # 1. Page Config & Aesthetic CSS
-st.set_page_config(page_title="Plant Care AI", page_icon="🌿")
+st.set_page_config(page_title="Plant Care AI", page_icon="🍃")
 
 st.markdown("""
     <style>
     /* Main background and font */
     .stApp {
-        background-color: #f0f7f4;
+        background-color: #C5D89D;
     }
     h1 {
-        color: #2d6a4f;
+        color: #043915;
         font-family: 'Inter', sans-serif;
     }
     .stButton>button {
-        background-color: #52b788;
+        background-color: #BBC863;
         color: white;
         border-radius: 20px;
         border: none;
@@ -25,7 +25,7 @@ st.markdown("""
     }
     /* Cute container for results */
     .result-card {
-        background-color: white;
+        background-color: #8BAE66;
         padding: 20px;
         border-radius: 15px;
         border-left: 10px solid #74c69d;
@@ -35,7 +35,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # 2. Sidebar / Header
-st.title("🌿 Plant Thirst Detector")
+st.title("🪴 Plant Thirst Detector")
 st.write("Upload a photo of your leaf or use the camera to see if it's thirsty!")
 
 # 3. Load the Model
@@ -92,4 +92,5 @@ if img_file is not None:
                 <p>Confidence: {round(confidence_score * 100)}%</p>
                 <p>Oh no! Your plant looks a bit thirsty. Time for a drink! 🚰</p>
             </div>
+
         """, unsafe_allow_html=True)
